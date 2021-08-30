0524b8005f1f69e9c6a99eaf1cadc1d32f4309f4
prajaktasutarbmis <89061585+prajaktasutarbmis@users.noreply.github.com>
2021-08-30 11:01:35 +0530

    Add files via upload

--- Js/project.js
+++ Js/project.js
@@ -1,17 +1,20 @@
+var temp={};
+var i=0;
 function saveData(){
 
-    var count=Math.round(Math.random()*100000);
-    localStorage.setItem('p'+count,count);
-    localStorage.setItem('title'+count, document.myform.title.value);
-    localStorage.setItem('description'+count,document.myform.description.value);
-    localStorage.setItem('logo'+count,document.myform.logo.value);
+  var count=Math.round(Math.random()*100000);
+  localStorage.setItem('p'+count,count);
+  localStorage.setItem('project_title'+count, document.getElementById('project_title').value);
+  localStorage.setItem('project_description'+count,document.getElementById('project_description').value);
+  localStorage.setItem('project_logo'+count,document.getElementById('project_logo').value);
+  temp[i]=count;
 
-    var formData = {};
-    formData["title"] = document.myform.title.value
-   formData["description"] = document.myform.description.value
-   formData["logo"] = document.myform.logo.value
-   insertNewRecord(formData,count);
-	
+  let formData = {};
+  formData["title"] = document.getElementById('project_title').value;
+ formData["description"] = document.getElementById('project_description').value;
+ formData["logo"] = document.getElementById('project_logo').value;
+ insertNewRecord(formData,count);
+
 	
   }
   
@@ -19,7 +22,7 @@ function saveData(){
   {
     var table = document.getElementById("tab").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-	cell1 = newRow.insertCell(0);
+	 cell1 = newRow.insertCell(0);
     cell1.innerHTML = count;
 	
     cell2 = newRow.insertCell(1);
@@ -32,32 +35,62 @@ function saveData(){
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.logo;
 
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<button type="button" class="btn btn-warning" onClick="editData(this)">Edit</button>
+                        <button type="button" class="btn btn-danger"  onClick="deleteData(this)">Delete</button>`;
+
+
  document.myform.reset();
-    
-	
   }
     
 
 function searchData(){
+    var id=document.getElementById('search').value;
+    let formData = {};
+    formData["title"] = localStorage.getItem('project_title'+id);
+    formData["description"] = localStorage.getItem('project_description'+id);
+    formData["logo"] = localStorage.getItem('project_logo'+id);
+    insertNewRecord(formData,id);
+}
+function deleteData(td){
+  selectedRow = td.parentElement.parentElement;
+  row = td.parentElement.parentElement;
+  // console.log(row);
+  var id=selectedRow.cells[0].innerHTML;
 
-    var id=document.myform.projectID.value;
+
+  var result=confirm("The project will be deleted,Are you sure?");
+  if(result==true)
+  {
+    localStorage.removeItem('p'+id);
+    localStorage.removeItem('project_title'+id);
+    localStorage.removeItem('project_description'+id);
+    localStorage.removeItem('project_logo'+id);
+    document.getElementById("tab").deleteRow(row.rowIndex);}
+
+}
+function editData(td){
+
+  /*selectedRow = td.parentElement.parentElement;
+  var id=selectedRow.cells[0].innerHTML;
+  document.myform.project_title.value = selectedRow.cells[1].innerHTML;
+  document.myform.project_description.value = selectedRow.cells[2].innerHTML;
+ 
+  var project_title=document.getElementById('project_title').value;
+  var project_description=document.getElementById('project_description').value;
+  localStorage.setItem('project_title'+id,project_title);
+  localStorage.setItem('project_description'+id,project_description);
+  selectedRow.cells[1].innerHTML = localStorage.getItem('project_title' + id);
+  selectedRow.cells[2].innerHTML = localStorage.getItem('project_description' + id);
+  document.myform.reset();*/
+
+
+/*    var id=document.myform.projectID.value;
+    var project_title=document.getElementById('project_title').value;
+    var project_description=document.getElementById('project_description').value;
+    localStorage.setItem('project_title'+id,project_title);
+    localStorage.setItem('project_description'+id,project_description);
     alert('Project title: '+localStorage.getItem('title'+id)+'\n'+'Project description '+localStorage.getItem('description'+id)+'\n'+'Project Logo: '+localStorage.getItem('logo'+id));
+*/
 }
-function deleteData(){
 
-    var id=document.myform.projectID.value;
-  localStorage.removeItem('p'+id);
-  localStorage.removeItem('title'+id);
-  localStorage.removeItem('description'+id);
-  localStorage.removeItem('logo'+id);
-  alert('Deleted');
-}
-function updateData(){
-
-    var id=document.myform.projectID.value;
-    var title=document.getElementById('title').value;
-    var description=document.getElementById('description').value;
-    localStorage.setItem('title'+id,title);
-    localStorage.setItem('description'+id,description);
-    alert('Project title: '+localStorage.getItem('title'+id)+'\n'+'Project description '+localStorage.getItem('description'+id)+'\n'+'Project Logo: '+localStorage.getItem('logo'+id));
-}
