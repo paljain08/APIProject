917527a5a147edb7bfc66b68553c4cbb18f17bc8
prajaktasutarbmis <89061585+prajaktasutarbmis@users.noreply.github.com>
2021-08-30 13:36:53 +0530

    new changes has added

--- Js/api.js
+++ Js/api.js
@@ -1,119 +1,108 @@
+var temp={};
+var i=0;
+function saveData(){
 
-function setData()
-  {
-	 
-   var count =document.myform.pid.value;
+  var count=document.getElementById('pid').value
   localStorage.setItem('p'+count,count);
-  localStorage.setItem('Url'+count,document.myform.url.value);
-  localStorage.setItem('Api_title'+count,document.myform.api_title.value);
-  localStorage.setItem('Method'+count,document.myform.method.value);
-  localStorage.setItem('Req'+count,document.myform.req.value);
-  localStorage.setItem('Res'+count,document.myform.res.value);
-  localStorage.setItem('Project'+count,document.myform.project.value);
-  localStorage.setItem('Module'+count,document.myform.module.value);
-  localStorage.setItem('Desc'+count,document.myform.desc.value);
+  localStorage.setItem('Project_Url'+count, document.getElementById('url').value);
+  localStorage.setItem('Project_title'+count, document.getElementById('api_title').value);
+  localStorage.setItem('Project_Method'+count, document.getElementById('method').value);
+  localStorage.setItem('Project_Req'+count, document.getElementById('req').value);
+  localStorage.setItem('Project_Res'+count, document.getElementById('res').value);
+  localStorage.setItem('Project_Module'+count, document.getElementById('module').value);
+  localStorage.setItem('Project_Desc'+count, document.getElementById('desc').value);
   
-  
-   
-   var formData = {};
-   formData["Url"] = document.myform.url.value
-   formData["Api_title"] = document.myform.api_title.value
-   formData["Method"] = document.myform.method.value
-   formData["Req"] = document.myform.req.value
-   formData["Res"] = document.myform.res.value
-   formData["Project"] = document.myform.project.value
-   formData["Module"] = document.myform.module.value
-   formData["Desc"] = document.myform.desc.value
-   
-   insertNewRecord(formData,count);
-	
+  temp[i]=count;
+
+  let formData = {};
+  formData["url"] = document.getElementById('url').value;
+  formData["api_title"] = document.getElementById('api_title').value;
+  formData["method"] = document.getElementById('method').value;
+  formData["req"] = document.getElementById('req').value;
+  formData["res"] = document.getElementById('res').value;
+  formData["module"] = document.getElementById('module').value;
+  formData["desc"] = document.getElementById('desc').value;
+  insertNewRecord(formData,count);
+
 	
   }
   
-  function insertNewRecord(data,count)
+   function insertNewRecord(data,count)
   {
     var table = document.getElementById("tab").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
+	
 	cell1 = newRow.insertCell(0);
     cell1.innerHTML = count;
 	
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.Url;
+    cell2.innerHTML = data.url;
 
+	
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.Api_title;
+    cell3.innerHTML = data.api_title;
 
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.Method;
- 
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = data.Req;
-
-    cell6 = newRow.insertCell(5);
-    cell6.innerHTML = data.Res;
+    cell4.innerHTML = data.method;
+	
+	cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.method;
+	
+	cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.req;
 	
 	cell7 = newRow.insertCell(6);
-    cell7.innerHTML = data.Project;
+    cell7.innerHTML = data.res;
+	
+	cell8 = newRow.insertCell(7);
+    cell8.innerHTML = data.module;
+	
+	
+	cell9 = newRow.insertCell(8);
+    cell9.innerHTML = data.desc;
 
-    cell8 = newRow.insertCell(7);
-    cell8.innerHTML = data.Module;
+    cell10 = newRow.insertCell(9);
+    cell10.innerHTML = `<button type="button" class="btn btn-warning" onClick="editData(this)">Edit</button>
+                        <button type="button" class="btn btn-danger"  onClick="deleteData(this)">Delete</button>`;
 
-    cell9 = newRow.insertCell(8);
-    cell9.innerHTML = data.Desc;
 
  document.myform.reset();
-    
-	
   }
   
   
- function removeData()
- {
-	 var id=document.myform.p_id.value;
-  localStorage.removeItem('p'+id);
-  localStorage.removeItem('Url'+id);
-  localStorage.removeItem('Api_title'+id);
-  localStorage.removeItem('Method'+id);
-  localStorage.removeItem('Req'+id);
-  localStorage.removeItem('Res'+id);
-  localStorage.removeItem('Project'+id);
-  localStorage.removeItem('Module'+id);
-  localStorage.removeItem('Desc'+id);
-  
-  alert("Project Deleted Successfully .....");
- }
- 
- function updateData()
- {
-	 var id=document.myform.p_id.value;
-	 var Url=document.myform.p_url.value;
-   var Api_title=document.myform.p_title.value
-   /*var Method=document.myform.p_method.value
-   var Req=document.myform.p_req.value
-   var Res=document.myform.p_res.value
-   var Project=document.myform.p_project.value
-   var Module=document.myform.p_module.value
-   var Desc=document.myform.p_desc.value */
-	 
-	  localStorage.setItem('Url'+id,Url);
-    localStorage.setItem('Api_title'+id,Api_title);
-     /*
-    localStorage.setItem('Method'+id,Method);
-    localStorage.setItem('Req'+id,Req);
-    localStorage.setItem('Res'+id,Res);
-    localStorage.setItem('Project'+id,Project);
-    localStorage.setItem('Module'+id,Module);
-    localStorage.setItem('Desc'+id,Desc);
-    
-     */
+  function searchData(){
+    var id=document.getElementById('search').value;
+    let formData = {};
+    formData["url"] = localStorage.getItem('Project_Url'+id);
+    formData["api_title"] = localStorage.getItem('Project_title'+id);
+    formData["method"]  = localStorage.getItem('Project_Method'+id);
+	formData["req"]  = localStorage.getItem('Project_Req'+id);
+	formData["res"]  = localStorage.getItem('Project_Res'+id);
+	formData["module"]= localStorage.getItem('Project_Module'+id);
+	formData["desc"]  = localStorage.getItem('Project_Desc'+id);
+    insertNewRecord(formData,id);
+}
 
-	  
-	  alert("Updated Url: "+Url +'\n'+'Updated Title: '+Api_title);
- }
- 
- function searchData()
- {
-	 var id=document.myform.p_id.value;
-	 var Url=document.myform.p_url.value;
-	 alert('Project Title: '+localStorage.getItem('p'+id)+'\n'+'Project URL: '+localStorage.getItem('Url'+id)+'\n'+'Project Title: '+localStorage.getItem('Api_title'+id));
- }
+function deleteData(td){
+  selectedRow = td.parentElement.parentElement;
+  row = td.parentElement.parentElement;
+  // console.log(row);
+  var id=selectedRow.cells[0].innerHTML;
+
+
+  var result=confirm("The project will be deleted,Are you sure?");
+  
+  selectedRow.cells[1].innerHTML = localStorage.getItem('Project_title' + id);
+  if(result==true)
+  {
+    localStorage.removeItem('p'+id);
+    localStorage.removeItem('Project_Url'+id);
+    localStorage.removeItem('Project_title'+id);
+    localStorage.removeItem('Project_Method'+id);
+	localStorage.removeItem('Project_Req'+id);
+	localStorage.removeItem('Project_Res'+id);
+	localStorage.removeItem('Project_Module'+id);
+	localStorage.removeItem('Project_Desc'+id);
+    document.getElementById("tab").deleteRow(row.rowIndex);}
+
+}
